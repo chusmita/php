409b3ee2ec43578fb2afea223d51ee4c94915fe9
chusmita <christianplenazzio@hotmail.com>
2022-07-11 19:08:49 -0300

    first commit

---
+++ clinica.php
@@ -0,0 +1,70 @@
+<?php
+//Definicion de pacientes
+$aPacientes = array();
+$aPacientes[] = array(
+    "dni" => "33.765.012",
+    "nombre" => "Ana Acuña",
+    "edad" => 45,
+    "peso" => 81.50,
+);
+$aPacientes[] = array(
+    "dni" => "23.684.385",
+    "nombre" => "Gonzalo Bustamante",
+    "edad" => 66,
+    "peso" => 79,
+);
+$aPacientes[] = array(
+    "dni" => "11.568.778",
+    "nombre" => "Martín Perez",
+    "edad" => 26,
+    "peso" => 77,
+);
+?>
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Clinica</title>
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+</head>
+
+<body>
+    <main class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1>Listado de Pacientes</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <table class="table table-hover border">
+                    <thead>
+                        <tr>
+                            <th>DNI</th>
+                            <th>Nombre y Apellido</th>
+                            <th>Edad</th>
+                            <th>Peso</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php for ($i = 0; si < count($aPacientes); $i++) { ?>
+                            <tr>
+                                <td><?php echo $paciente["dni"];  ?></td>
+                                <td><?php echo $paciente["nombre"];  ?></td>
+                                <td><?php echo $paciente["edad"];  ?></td>
+                                <td><?php echo $paciente["peso"];  ?></td>
+                            </tr>
+                        <?php } ?>
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </main>
+</body>
+
+</html>
